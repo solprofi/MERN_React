8c791f7e51ccf9bbae262c57ff84616e5d815bb9
Roman Sologor <solohor.roman@gmail.com>
2019-02-18 14:52:09 +0200

    added registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,51 @@
 const express = require('express');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+const User = require('../../models/User');
 
 //@route  GET api/users/test
 //@desc   Tests users route
 //@access Public
 router.get('/test', (req, res) => res.json({ msg: 'Users route works' }));
+
+//@route  Post api/users/register
+//@desc   Register a user
+//@access Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        res.status(400).json({ email: 'Email already exists' });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', //size
+          r: 'pg', //rating
+          d: 'mm', //default to no avatar outline
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          avatar,
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) {
+              throw err;
+            } else {
+              newUser.password = hash;
+              newUser.save()
+                .then(user => res.json(user))
+                .catch(err => console.error(err))
+            }
+          })
+        })
+      }
+    })
+
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
