b0f1d4e1d45174b8f500aa42553f2f4e7e8d03cd
Roman Sologor <solohor.roman@gmail.com>
2019-02-19 15:32:46 +0200

    added a route to delete user and his profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -290,4 +290,16 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', { session: fals
     .catch(err => res.status(404).json(err));
 });
 
+//@route  DELETE api/profile
+//@desc   Delete user and profile
+//@access Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile
+    .findOneAndRemove({ user: req.user.id })
+    .then(() => {
+      User
+        .findOneAndRemove({ _id: req.user.id })
+        .then(() => res.json({ success: true }));
+    });
+});
 module.exports = router;
