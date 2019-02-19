02acca0b20c2ed5c7f9d2fed50fdf9cc96d3b17f
Roman Sologor <solohor.roman@gmail.com>
2019-02-19 11:12:27 +0200

    added create and update profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -32,4 +32,61 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err));
 });
 
+//@route  POST api/profile
+//@desc   Create or edit user profile
+//@access Protected
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const userProfile = {};
+  const { body } = req;
+
+  if (body.handle) userProfile.handle = body.handle;
+  if (body.company) userProfile.company = body.company;
+  if (body.website) userProfile.website = body.website;
+  if (body.location) userProfile.location = body.location;
+  if (body.bio) userProfile.bio = body.bio;
+  if (body.status) userProfile.status = body.status;
+  if (body.githubUserName) userProfile.githubUserName = body.githubUserName;
+
+  // skills - split into array 
+  if (typeof body.skills !== 'undefined') {
+    userProfile.skills = body.skills.split(',');
+  }
+
+  userProfile.social = {};
+  if (body.youtube) userProfile.social.youtube = body.youtube;
+  if (body.facebook) userProfile.social.facebook = body.facebook;
+  if (body.instagram) userProfile.social.instagram = body.instagram;
+  if (body.linkedin) userProfile.social.linkedin = body.linkedin;
+  if (body.twitter) userProfile.social.twitter = body.twitter;
+
+  Profile
+    .findOne({ user: body.id })
+    .then(profile => {
+      if (profile) {
+        // Update
+        Profile
+          .findOneAndUpdate({ user: body.id }, { $set: userProfile }, { new: true })
+          .then(profile => res.json(profile));
+      } else {
+        // Create
+
+        // Check if handle exists
+
+        Profile
+          .findOne({ handle: userProfile.handle })
+          .then(profile => {
+            if (profile) {
+              errors.handle = 'Handle already exists';
+              res.status(400).json(errors);
+            } else {
+              new Profile(userProfile)
+                .save()
+                .then(profile => res.json(profile));
+            }
+          })
+      }
+    });
+
+});
+
 module.exports = router;
