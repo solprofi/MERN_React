471fc88cda0271206f187b5d10b390d1049b68b9
Roman Sologor <solohor.roman@gmail.com>
2019-02-19 16:28:57 +0200

    added routes to get all posts and get a post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -37,4 +37,26 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   newPost.save().then(post => res.json(post));
 });
 
+//@route  GET api/posts
+//@desc   Get all posts
+//@access Public
+router.get('/', (req, res) => {
+  Post
+    .find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404));
+});
+
+//@route  GET api/posts/:id
+//@desc   Get post by id
+//@access Public
+router.get('/:id', (req, res) => {
+  Post
+    .findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404));
+});
+
+
 module.exports = router;
