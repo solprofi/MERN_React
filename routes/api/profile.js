5d7d67e12dcafc24d798dea03222d9a8f9b404e8
Roman Sologor <solohor.roman@gmail.com>
2019-02-19 10:46:54 +0200

    added a route to check current users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load models
+
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
 
 //@route  GET api/profile/test
 //@desc   Tests profile route
 //@access Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile route works' }));
+
+//@route  GET api/profile
+//@desc   Get current profile
+//@access Protected
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+
+  Profile
+    .findOne({ user: req.body.user })
+    .then(profile => {
+      if (profile) {
+        res.json(profile);
+      } else {
+        errors.noProfile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+    })
+    .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
