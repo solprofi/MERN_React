c9973b26cb60ab5157b96077e19763f988bdecda
Roman Sologor <solohor.roman@gmail.com>
2019-02-20 12:24:42 +0200

    added routes to login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <div className="container">
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
