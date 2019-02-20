834a3bcd5b9dda3146c0e747e9df177d81100b9e
Roman Sologor <solohor.roman@gmail.com>
2019-02-20 14:35:13 +0200

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <div className="container">
-            <Route path='/login' component={Login} />
-            <Route path='/register' component={Register} />
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <div className="container">
+              <Route path='/login' component={Login} />
+              <Route path='/register' component={Register} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
